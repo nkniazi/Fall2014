e9d7344f947e67e62b26e1163a7d6c3d03c88424
nkniazi <alnasrkhan@gmail.com>
2014-08-28 13:40:03 +0500

    it is first

---
+++ first/src/pk/edu/iba/first/MainActivity.java
@@ -0,0 +1,15 @@
+package pk.edu.iba.first;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
